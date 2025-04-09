3809160f927174ea024b66de5c1e82d97c6d9b35
Francisco Torres <ftorres7382@gmail.com>
2025-04-09 01:42:15 -0400

    Change config file to be a dictionary this way the settings can be repalced later on

--- config.py
+++ config.py
@@ -1,16 +1,81 @@
-from app_code.enums import *
+'''
+Using a python file as a config because it is the easiest way to create a config file that has good documentation
+
+Plus it is easier to edit and understand than a json or yaml file
+
+This could have issues in the furture with safe loading
+    To fix this we can strictly enforce the class structures
+    possibly make objects from them safe load only what we need
+
+ORRR make a mini program that creates the configuration and checks all the settings to make sure they are correct
+
+'''
+
+from app_code.enums import VALID_MODEL_NAMES, VALID_AGENT_CLASSES
 from app_code.agents.secretary_agent import secretary_agent
 
-# ollama settings
-ollama_url = "http://localhost"
-ollama_api_port: int =  11434 
-complete_ollama_api_url = f"{ollama_url}:{ollama_api_port}"
+import typing as t
 
-# app settings
-requirements_check_verbose = True
+class SETTINGS_DICT(t.TypedDict):    
+    ##########################
+    # Build and Requirements Validation Settings
+    ##########################
+    # region:
+    requirements_filepath: str
+    venv_folderpath: str
+    # endregion
 
-# This is the agent that will be run
-selected_agent = secretary_agent(
-    name="Quinn",
-    model_name=VALID_MODEL_NAMES.deepseek_r1_14b
-)
+
+    ##########################
+    # Ollama Settings
+    ##########################
+    # region:
+    ollama_url: str
+    ollama_api_port: int
+    complete_ollama_api_url: t.Union[str, None]
+    # endregion
+
+
+    ##########################
+    # Agents Settings
+    ##########################
+    # region:
+    agent_object: t.Literal[
+        VALID_AGENT_CLASSES.secretary_agent
+        ]
+    # endregion
+
+
+settings = {
+    ##########################
+    # Build and Requirements Validation Settings
+    ##########################
+    # region:
+    "requirements_filepath": "./requirements.txt",
+    "venv_folderpath": ".venv",
+    # endregion
+
+    ##########################
+    # Ollama Settings
+    ##########################
+    # region:
+    "ollama_url": "http://localhost",
+    "ollama_api_port": 11434,
+
+    # This will be completely filled in later
+    "complete_ollama_api_url": None,
+    # endregion
+
+    ##########################
+    # Agents Settings
+    ##########################
+    # region:
+    "agent_object": secretary_agent(
+        name="Quinn",
+        model_name=VALID_MODEL_NAMES.deepseek_r1_14b
+    )
+    # endregion
+}
+
+# Fill in the missing values
+settings['ollama_url'] = f"{settings['ollama_url']}:{settings['ollama_api_port']}"
